8a7b664efa94b4a5553722615d2a8c6b5455b91b
ThePope05 <swobben05@gmail.com>
2025-11-03 12:01:49 +0100

    Removed element list

--- assets/scripts/screen_element_controller.gd
+++ assets/scripts/screen_element_controller.gd
@@ -1,6 +1,6 @@
 extends Panel
 
-@onready var element_screen : Control = $HSplitContainer/ElementPanel/Background
+@onready var element_screen : Control = $ElementPanel/Background
 
 func _add_element_to_screen(element : DraggableTextureRect) -> void:
 	element_screen.add_child(element)
