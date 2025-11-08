78cc2abfbb5ffc1090efb5afe9bf7a09e9b83024
Robert-Jan Ruhof <robert.ruhof@gmail.com>
2025-11-08 15:57:02 +0100

    Added healthbar with camera shake effect

--- assets/scripts/customNodes/MoveableCamera.gd
+++ assets/scripts/customNodes/MoveableCamera.gd
@@ -1,6 +1,9 @@
 extends Camera3D
 class_name MoveableCamera3D
 
+var is_shaking = false
+var shake_amount = 0.02
+var original_position: Vector3
 @export var mouse_influence: float = 0.5
 @export var rotation_speed: float = 5.0
 @export var min_rotation_limit: Vector3 = Vector3(-10, -20, 0)
@@ -12,6 +15,7 @@ var _target_rotation: Vector3
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	_target_rotation = rotation_degrees
+	original_position = position
 
 func _process(delta):
 	var viewport_size = get_viewport().get_visible_rect().size
@@ -41,3 +45,17 @@ func _process(delta):
 
 	# Apply rotation smoothly
 	rotation_degrees = _target_rotation
+	if is_shaking:
+		position = original_position + Vector3(
+			randf_range(-shake_amount, shake_amount),
+			randf_range(-shake_amount, shake_amount),
+			0
+		)
+	else:
+		position = original_position
+	
+func _on_healthbar_start_shaking():
+	is_shaking = true
+
+func _on_healthbar_stop_shaking():
+	is_shaking = false
