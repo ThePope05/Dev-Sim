6e234a3fafd86530f1fba6f2fafc3c384b468d38
ThePope05 <swobben05@gmail.com>
2025-11-09 22:07:57 +0100

    Added wallpaper for main screen

--- assets/scripts/screen_element_controller.gd
+++ assets/scripts/screen_element_controller.gd
@@ -1,13 +1,15 @@
 extends Panel
 
-@onready var element_screen : Control = $Background
+@onready var element_screen : ColorRect = $Background
+@onready var wallpaper: MarginContainer = $Background/Wallpaper
 
 func _add_element_to_screen(element : Control) -> void:
 	element_screen.add_child(element)
 
 func _start_assignment(name: String) -> void:
 	for child in element_screen.get_children():
-		element_screen.remove_child(child)
+		if not child.name.containsn("wallpaper"):
+			element_screen.remove_child(child)
 		
 	var design : PackedScene
 	
@@ -23,8 +25,14 @@ func _start_assignment(name: String) -> void:
 	if design == null:
 		return
 	
+	element_screen.color = "#fff"
+	wallpaper.visible = false
 	element_screen.add_child(design.instantiate())
 
 func _stop_assignment() -> void:
 	for child in element_screen.get_children():
-		element_screen.remove_child(child)
+		if not child.name.containsn("wallpaper"):
+			element_screen.remove_child(child)
+	
+	element_screen.color = "#000"
+	wallpaper.visible = true
